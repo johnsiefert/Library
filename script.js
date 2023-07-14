970585094c756667884e7ae51d7724ce1ac0b539
John <johnsief12@gmail.com>
2023-07-14 17:07:40 -0400

    messing with the grid to display in row

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const addBookBtn = document.querySelector("#add");
-let myLibrary = [];
+const addBookBtn = document.querySelector("[data-open-modal]");
+const submitBtn = document.querySelector("#submit");
+const modal =document.querySelector("[data-modal]");
+const booksGrid = document.querySelector("#booksGrid")
+let myLibrary = ["kk","kksdfa"];
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -21,48 +24,49 @@ function addBookToLibrary() {
  myLibrary.push(addBook);
 }
 
-addBookBtn.addEventListener("click", function(){
-    addBookToLibrary();
-    console.log(myLibrary);
-    for(let i = 0; i < myLibrary.length; i++) {
-        //form Container
-        let formContainer = document.createElement("DIV");
-        formContainer.setAttribute("class", "formContainer");
-        document.body.appendChild(formContainer);
-        //form
-        let form = document.createElement("FORM");
-        formContainer.appendChild(form);
-        form.setAttribute("id", "form");
-        let formTitle = document.createElement("H2");
-        formTitle.setAttribute("id", "formTitle");
-        formTitle.textContent = "Book Information";
-        form.appendChild(formTitle);
-        //form inputs
-        let formTitleInput = document.createElement("INPUT");
-        formTitleInput.setAttribute("type", "text");
-        formTitleInput.setAttribute("id", "title");
-        formTitleInput.setAttribute("placeholder", "Title");
-        form.appendChild(formTitleInput);
-        //author inputs
-        let formAuthorInput = document.createElement("INPUT");
-        formAuthorInput.setAttribute("type", "text");
-        formAuthorInput.setAttribute("id", "author");
-        formAuthorInput.setAttribute("placeholder", "Author");
-        form.appendChild(formAuthorInput);
-        //pages inputs
-        let formPagesInput = document.createElement("INPUT");
-        formPagesInput.setAttribute("type", "text");
-        formPagesInput.setAttribute("id", "pages");
-        formPagesInput.setAttribute("placeholder", "Pages");
-        form.appendChild(formPagesInput);
-    //submit button
-        let submit = document.createElement("BUTTON");
-        submit.setAttribute("type", "submit");
-        submit.setAttribute("id", "submit");
-        submit.textContent = "Submit";
-        form.appendChild(submit);
-    }
 
+
+
+addBookBtn.addEventListener("click", () => {
+modal.showModal()
 });
+
+function bookContainer() {
+console.log(myLibrary);
+for(let i = 0; i < myLibrary.length; i++) {
+
+    //form Container
+    let formContainer = document.createElement("DIV");
+    formContainer.setAttribute("class", "books-grid");
+    document.body.appendChild(formContainer);
+
+    //form
+    let form = document.createElement("FORM");
+    formContainer.appendChild(form);
+    form.setAttribute("class", "book-card");
+    let formTitle = document.createElement("H2");
+    formTitle.textContent = "Book Information";
+    form.appendChild(formTitle);
+    //form inputs
+    let formTitleInput = document.createElement("INPUT");
+    formTitleInput.setAttribute("type", "text");
+    formTitleInput.setAttribute("class", "book-card");
+    formTitleInput.setAttribute("placeholder", "Title");
+    form.appendChild(formTitleInput);
+    //author inputs
+    let formAuthorInput = document.createElement("INPUT");
+    formAuthorInput.setAttribute("type", "text");
+    formAuthorInput.setAttribute("class", "book-card");
+    formAuthorInput.setAttribute("placeholder", "Author");
+    form.appendChild(formAuthorInput);
+    //pages inputs
+    let formPagesInput = document.createElement("INPUT");
+    formPagesInput.setAttribute("type", "text");
+    formPagesInput.setAttribute("class", "book-card");
+    formPagesInput.setAttribute("placeholder", "Pages");
+    form.appendChild(formPagesInput);
+}
+}
+bookContainer()
 
 
